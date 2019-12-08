dadb087aa9a48ca2aab3d27eeb916737240e6982
Jens Raaby <jensraaby@users.noreply.github.com>
2019-12-08 12:41:01 +0000

    Day 7 in Go - Add feedback loop for part 2
    Took a while to work out how to pipe things through the circuit and had to look at someone else's implementation for how to halt the whole process. This was a useful lesson in using goroutines and channels!

--- go/day7/AmplificationCircuit.go
+++ go/day7/AmplificationCircuit.go
@@ -13,10 +13,12 @@ func main() {
 	testProgram := []int{3, 9, 8, 9, 10, 9, 3, 9, 4, 9, 99, -1, 8}
 	inputChan := make(chan int)
 	outputChan := make(chan int)
-	go ExecuteProgram(testProgram, inputChan, outputChan)
+	haltChan := make(chan bool)
+	go ExecuteProgram(testProgram, inputChan, outputChan, haltChan)
 	inputChan <- 9
 	inputChan <- 8
 	output := <-outputChan
+	<-haltChan
 	fmt.Printf("Finished test execution. Output: %v\n", output)
 
 	testProg1 := []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}
@@ -35,12 +37,31 @@ func main() {
 	sig := runAmplificationCircuit(inputProgram, 0, 0, 1, 2, 3, 4)
 	fmt.Printf("Output from part 1 prog: %v\n", sig)
 
-	phaseInputs := []int{0, 1, 2, 3, 4}
+	phaseInputsPart1 := []int{0, 1, 2, 3, 4}
+	findBestPhases(phaseInputsPart1, inputProgram)
+
+	fmt.Println("###################################")
+	fmt.Println("PART 2:")
+	testProg := []int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5}
+	resultTestP2 := runAmplificationCircuit(testProg, 0, 9, 8, 7, 6, 5)
+	fmt.Printf("Result for test example 1: %v\n", resultTestP2)
+
+	testProg = []int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+		-5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+		53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10}
+	resultTestP2 = runAmplificationCircuit(testProg, 0, 9, 7, 8, 5, 6)
+	fmt.Printf("Result for test example 2: %v\n", resultTestP2)
+
+	phaseInputsPart2 := []int{5, 6, 7, 8, 9}
+	findBestPhases(phaseInputsPart2, inputProgram)
+}
+
+func findBestPhases(initialPhaseValues []int, inputProgram []int) (chan []int, [][]int, []int, int) {
 	phasePermutationChan := make(chan []int)
 	phasePermutations := make([][]int, 1)
-	bestPermutation := phaseInputs
+	bestPermutation := initialPhaseValues
 	bestResult := 0
-	go permutations(phaseInputs, phasePermutationChan)
+	go permutations(initialPhaseValues, phasePermutationChan)
 	for i := range phasePermutationChan {
 		phasePermutations = append(phasePermutations, i)
 		result := runAmplificationCircuit(inputProgram, 0, i[0], i[1], i[2], i[3], i[4])
@@ -49,62 +70,61 @@ func main() {
 			bestPermutation = i
 		}
 	}
-	fmt.Printf("Best result: %v for permutation: %v", bestResult, bestPermutation)
+	fmt.Printf("Best result: %v for permutation: %v\n", bestResult, bestPermutation)
+	return phasePermutationChan, phasePermutations, bestPermutation, bestResult
 }
 
-func runAmplificationCircuit(software []int, inputSignal int, phase1, phase2, phase3, phase4, phase5 int) int {
-	amplifierA := makeAmplifier(software)
-	amplifierB := makeAmplifier(software)
-	amplifierC := makeAmplifier(software)
-	amplifierD := makeAmplifier(software)
-	amplifierE := makeAmplifier(software)
+func runAmplificationCircuit(software []int, inputSignal, phase1, phase2, phase3, phase4, phase5 int) int {
+	eToA := make(chan int, 1) // size 1 so it waits for sig from E
+	aToB := make(chan int)
+	bToC := make(chan int)
+	cToD := make(chan int)
+	dToE := make(chan int)
 
-	amplifierA.startAmplifier(phase1, inputSignal)
-	amplifierB.startAmplifier(phase2, <-amplifierA.output)
-	amplifierC.startAmplifier(phase3, <-amplifierB.output)
-	amplifierD.startAmplifier(phase4, <-amplifierC.output)
-	signal := <-amplifierE.startAmplifier(phase5, <-amplifierD.output)
-	return signal
-}
+	halt := make(chan bool)
 
-type amplifier struct {
-	program []int
-	input   chan int
-	output  chan int
-}
+	// wire together the inputs and outputs
+	go ExecuteProgram(software, eToA, aToB, halt)
+	go ExecuteProgram(software, aToB, bToC, halt)
+	go ExecuteProgram(software, bToC, cToD, halt)
+	go ExecuteProgram(software, cToD, dToE, halt)
+	go ExecuteProgram(software, dToE, eToA, halt)
 
-func makeAmplifier(program []int) *amplifier {
-	return &amplifier{
-		program: program,
-		input:   make(chan int),
-		output:  make(chan int),
+	// send phase values to inputs
+	eToA <- phase1
+	aToB <- phase2
+	bToC <- phase3
+	cToD <- phase4
+	dToE <- phase5
+
+	// send initial signal to first amplifier
+	eToA <- inputSignal
+
+	// wait for all 5 amplifiers to send halt signal
+	for i := 0; i < 5; i++ {
+		<-halt
 	}
+
+	// the result is the output of the last amplifier
+	return <-eToA
 }
 
-func (amp *amplifier) startAmplifier(phaseSetting int, inputSignal int) chan int {
-	go ExecuteProgram(amp.program, amp.input, amp.output)
-	amp.input <- phaseSetting
-	amp.input <- inputSignal
-	return amp.output
-}
-
-func ExecuteProgram(memory []int, input chan int, outputChan chan int) {
+func ExecuteProgram(memory []int, input chan int, outputChan chan int, halt chan bool) {
 	// Create copy of memory rather than mutating in place
 	newMemory := make([]int, len(memory))
 	copy(newMemory, memory)
 
 	instructionPointer := 0
-	stopped := false
 
-	for !stopped {
+	for {
 		instruction, err := parseProgram(memory[instructionPointer])
 		if err != nil {
 			// give up if we can't parse the program
 			panic(err)
 		}
-		//fmt.Printf("Opcode: %v\n Mode 1: %v Mode 2: %v\n", instruction.opcode, instruction.mode1, instruction.mode2)
 		if instruction.opcode == 99 {
-			stopped = true
+			halt <- true
+			return
 		} else {
 			// load params from memory. This could fail if the mode is "position" and the index is then out of bounds
 			param1, param1err := readFromMemory(instruction.mode1, instructionPointer+1, newMemory)
