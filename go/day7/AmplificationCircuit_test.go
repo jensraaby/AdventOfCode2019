1a76831b105c2c791cab739c443f023952a98f06
Jens Raaby <jensraaby@users.noreply.github.com>
2019-12-09 13:59:47 +0000

    Fix test for Day 7 Go

--- go/day7/AmplificationCircuit_test.go
+++ go/day7/AmplificationCircuit_test.go
@@ -7,11 +7,13 @@ import (
 func TestInputOutput(t *testing.T) {
 	inputChan := make(chan int)
 	outputChan := make(chan int)
+	haltChan := make(chan bool)
 	input := 3
 	inputProgram := []int{3, 0, 4, 0, 99}
-	go ExecuteProgram(inputProgram, inputChan, outputChan)
+	go ExecuteProgram(inputProgram, inputChan, outputChan, haltChan)
 	inputChan <- input
 	result := <-outputChan
+	<-haltChan
 
 	if result != input {
 		t.Errorf("Output should be same as input (%v should be %v)", result, input)
