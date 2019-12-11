05b9ac4dc5c845eb1af475bedb15e87b5b822743
Jens Raaby <jensraaby@users.noreply.github.com>
2019-12-11 21:39:24 +0000

    Day 10 part 2 (Scala) - very messy this - I know could be done in a more slick way

--- scala/src/main/scala/day10/MonitoringStation.scala
+++ scala/src/main/scala/day10/MonitoringStation.scala
@@ -16,8 +16,8 @@ object MonitoringStation extends IOApp {
     }
 
     def angleTo(other: Asteroid): Double = {
-      val angle = -Math.atan2(other.y - y, other.x - x)
-      if (angle >= -Math.PI / 2) angle else angle + (2 * Math.PI)
+      // flipped x and y so angle is from y axis not x axis
+      -Math.atan2(other.x - x, other.y - y)
     }
 
     def visibleFrom(asteroids: Set[Asteroid]): Int = {
@@ -31,6 +31,35 @@ object MonitoringStation extends IOApp {
 
   def bestAsteroidToUseForMonitoring(asteroids: Set[Asteroid]): Asteroid = {
     asteroids.maxBy(_.visibleFrom(asteroids))
+  }
+
+  def destructionOrder(monitoringStationAsteroid: Asteroid,
+                       asteroids: Set[Asteroid]) = {
+    val asteroidsWithoutMonitoringStation = asteroids - monitoringStationAsteroid
+    val toDestroy = mutable.HashMap[Double, List[Asteroid]]()
+
+    asteroidsWithoutMonitoringStation.foreach { asteroid =>
+      toDestroy.updateWith(monitoringStationAsteroid.angleTo(asteroid)) {
+        case Some(lst) => Some(asteroid :: lst)
+        case None      => Some(List(asteroid))
+      }
+    }
+
+    toDestroy.keys.foreach { angle =>
+      toDestroy.updateWith(angle) {
+        case Some(lst) =>
+          Some(lst.sortBy(_.distanceTo(monitoringStationAsteroid)))
+        case None => None
+      }
+    }
+
+    val destroyed = mutable.ListBuffer[Asteroid]()
+    toDestroy.keys.toList.sorted.foreach { angle =>
+      val closestAsteroid :: rest = toDestroy(angle)
+      toDestroy.update(angle, rest)
+      destroyed.append(closestAsteroid)
+    }
+    destroyed
   }
 
   def parseGrid(inputLines: Vector[String]): IO[mutable.HashSet[Asteroid]] =
@@ -73,6 +102,16 @@ object MonitoringStation extends IOApp {
       _ <- Console.putStrLn(
         s"Number of visible asteroids from $bestAsteroid: ${bestAsteroid
           .visibleFrom(testGrid.toSet)}"
+      )
+      twoHundredthAsteroidToDestroy = destructionOrder(
+        bestAsteroidToUseForMonitoring(testGrid.toSet),
+        testGrid.toSet
+      )(199)
+      _ <- Console.putStrLn(
+        s"200th asteroid to destroy: ${twoHundredthAsteroidToDestroy}"
+      )
+      _ <- Console.putStrLn(
+        s"Part 2 solution: ${twoHundredthAsteroidToDestroy.x * 100 + twoHundredthAsteroidToDestroy.y}"
       )
     } yield ExitCode.Success
 }
